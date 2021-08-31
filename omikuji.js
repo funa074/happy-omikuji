df3d9dd37a94b2507b63a8725abd7efb4be9c733
funa074 <fnk074@gmail.com>
2021-08-31 23:27:03 +0900

    テキストの内容を変更する関数、イベントを作成

--- omikuji.js
+++ omikuji.js
@@ -4,10 +4,15 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-let btn = document.getElementsByClassName("btn").innerHTML;
-btn.addEventListener("click", omikuji[getRandomInt(5)]);
+let btn = document.getElementsByClassName("btn")[0];
 
-console.log(omikuji[getRandomInt(5)]);
+function text(id, text) {
+  id.innerHTML = text;
+}
+
+btn.onclick = function () {
+  text(btn, omikuji[getRandomInt(5)]);
+};
 
 // let omikuji0 = "大吉";
 // let omikuji1 = "吉";
