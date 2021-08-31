430c3be96853f5e4e6ff1a63c89cf2241c1ff11c
hosimiya7 <nerokarurin@gmail.com>
2021-08-31 23:41:22 +0900

    text関数削除

--- omikuji.js
+++ omikuji.js
@@ -6,17 +6,6 @@ function getRandomInt(max) {
 
 let btn = document.getElementsByClassName("btn")[0];
 
-function text(id, text) {
-  id.innerHTML = text;
+btn.onclick = function() {
+  btn.innerHTML = omikuji[getRandomInt(5)]
 }
-
-btn.onclick = function () {
-  text(btn, omikuji[getRandomInt(5)]);
-};
-
-// let omikuji0 = "大吉";
-// let omikuji1 = "吉";
-// let omikuji2 = "凶";
-
-// const a = new Number();
-// console.log(a.toString(getRandomInt(3)));
