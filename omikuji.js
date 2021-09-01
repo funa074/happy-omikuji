94d4439e181b960dbb6850c8d2c0894d91d86fdc
funa074 <fnk074@gmail.com>
2021-09-01 22:46:34 +0900

    Complete

--- omikuji.js
+++ omikuji.js
@@ -1,22 +1,13 @@
 let omikuji = ["超大吉", "大大吉", "大吉", "吉", "末吉"];
+let btn = document.querySelector(".btn");
+btn.setAttribute("onclick", "omikuiOnclick()");
+let omikuji_class = document.querySelector(".omikuji");
+
+function omikuiOnclick() {
+  console.log(omikuji[getRandomInt(5)]);
+  omikuji_class.textContent = omikuji[getRandomInt(5)];
+}
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
-
-let btn = document.getElementsByClassName("btn")[0];
-
-function text(id, text) {
-  id.innerHTML = text;
-}
-
-btn.onclick = function () {
-  text(btn, omikuji[getRandomInt(5)]);
-};
-
-// let omikuji0 = "大吉";
-// let omikuji1 = "吉";
-// let omikuji2 = "凶";
-
-// const a = new Number();
-// console.log(a.toString(getRandomInt(3)));
